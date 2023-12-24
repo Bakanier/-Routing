16711a60c04f3ca9f87ce8e0e26790fddd79ffbd
Bakanier <kushnazaruul@gmail.com>
2023-12-24 22:51:40 +0600

    Routing

---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const About = () => {
+  return <div><h2>О нас.</h2><p>Мы - хорошо слаженная и дружная команда!</p></div>;
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Contact = () => {
+  return <div><h2>Свяжитесь с нами!</h2><p>Смело обращайтесь!</p></div>;
+};
+
+export default Contact;
